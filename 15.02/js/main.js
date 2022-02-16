f1fa740b489861d83cd1cbc8d3df705cb173cf47
Ataxan  Ismailov <atmospheremg63@gmail.com>
2022-02-16 10:58:36 +0600

    first commit

---
+++ 15.02/js/main.js
@@ -0,0 +1,4 @@
+// let name = 'Ataxan';
+// let surName = 'Ismailov';
+// surName = name;
+// alert(surName);
---
+++ 16.02/js/main.js
@@ -0,0 +1,12 @@
+// let camelCase = 1;
+// let snake_case = 2;
+// let PascalCase = 3;
+// alert(camelCase);
+// alert(snake_case);
+// alert(PascalCase);
+
+// let str1 = 'SALAM';
+// let str2 = 'BISHKEK';
+// alert(`${str1}, ${str2}`);
+
+
