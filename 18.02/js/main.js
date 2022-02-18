302f4a4f2d8ebd8f47486558bc174f4f9e8596c7
Ataxan  Ismailov <atmospheremg63@gmail.com>
2022-02-18 11:46:56 +0600

    rrr

---
+++ 18.02/js/main.js
@@ -0,0 +1,37 @@
+// ! 111111111
+let num1 = 10;
+num++;
+
+console.log(num);
+
+let num = 10;
+num--;
+console.log(num);
+
+
+// ! 222222222
+let name = 'Ataxan';
+alert('Hi ' + name);
+
+// ! 3333333333333
+let age = 20;
+alert('Mne ' + age + ' imya!');
+
+
+// !444444444
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result)
+
+// !555555555
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
